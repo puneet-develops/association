2a00230eb8582193a0a046e1eafdff9e0d9d7e4e
puneet-develops <puneet.k@antino.com>
2024-03-07 10:23:05 +0530

    .

--- models/product.js
+++ models/product.js
@@ -4,11 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       this.belongsToMany(models.User, { through: 'Wishlist', foreignKey: 'product_id' });
       this.hasMany(models.OrderItem, {as:'orderItems', foreignKey: 'product_id' });
